190ab9d2ac9da1f1e6f81a92f2a72406c517e933
maxkiley <student@maxkiley.local>
2016-01-14 11:08:05 -0600

    initial commit

--- Table View Practice/ViewController.swift
+++ Table View Practice/ViewController.swift
@@ -1,25 +1,33 @@
-//
-//  ViewController.swift
-//  Table View Practice
-//
-//  Created by maxkiley on 1/13/16.
-//  Copyright © 2016 maxkiley. All rights reserved.
-//
 
 import UIKit
+//add tableViewDataSource and Delegate protocols
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var myTableView: UITableView!
+    var superHeros = ["Batman", "Wonderwoman", "Superman", "Flash", "Aquaman", "Scuba Steve"]
+    var realNames = ["Bruce Wayne", "Diana", "Clark Kent", "Barry Allen", "Arthur Curry", "Steve"]
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //setting datasource and delegate to the view controller
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //creating a cell that would store your data on a tableView
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myTableViewCell = myTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
+        myTableViewCell.textLabel?.text = superHeros[indexPath.row]
+        myTableViewCell.detailTextLabel?.text = realNames[indexPath.row]
+        return myTableViewCell
     }
-
-
+    //sets the nuber of rows in your tableView
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+    
+        return superHeros.count
+    }
 }
 
